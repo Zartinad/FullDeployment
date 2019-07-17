2be65c5708d41277fa99fb86258849185ff9b475
Adam <a.zartin@mail.utoronto.ca>
2019-07-16 22:49:10 -0400

    installDependancies

--- installDependancies.sh
+++ installDependancies.sh
@@ -1,5 +1,20 @@
 #Stage updates
-apt-get update
+sudo apt update
+
+#Install nginx and set up firewall
+#Note -y forces yes to any prompts about installations
+sudo apt install -y nginx
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status
+
+#Install MySQL
+#sudo apt install mysql-server
+#sudo mysql_secure_installation
+
+#Install php-fpm, process manager
+sudo add-apt-repository -y universe
+sudo apt install -y php-fpm php-mysql
+
 
 #Update Ubuntu to download new versions of node properly.
 #This was due to a problem where ubuntu would download an 
@@ -9,10 +24,10 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 #Install node, npm, git
 #Note: nodejs includes npm and node already
-sudo apt-get install nodejs git
+sudo apt-get -y install nodejs git
 
 #Install pm2, a process management tool for node
-npm install pm2@latest -g
+npm install -y pm2@latest -g
 
 echo  "\n ======PACKAGE VERSIONS====== \n"
 
@@ -25,3 +40,7 @@ echo "Git Version:"
 git --version
 echo "PM2 Version:"
 pm2 --version
+
+echo "\n \n"
+echo "You can now access your default nginx website here"
+curl -4 icanhazip.com
