5c23c4c7c514a390059b7e57bfd7f166a92d1bfe
Adam <a.zartin@mail.utoronto.ca>
2019-07-18 16:33:57 -0400

    phpmyadmin

---
+++ installphpMyAdmin.sh
@@ -0,0 +1,5 @@
+
+sudo apt install -y phpmyadmin
+sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+
+echo "phpmyadmin is now available to access :)"
---
+++ mysql.js
@@ -0,0 +1,8 @@
+module.exports = mysql_config = {
+      host:'138.51.240.116',
+      user:'app',
+      password:'adam',
+    database: live,
+    multipleStatements: true,
+    connectionLimit : 10
+}
