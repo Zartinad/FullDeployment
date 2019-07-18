2ba2a4b0ecb7ff720b9f587643f27a3a43188e8b
Adam <a.zartin@mail.utoronto.ca>
2019-07-18 14:10:29 -0400

    frontend scripts

---
+++ sample-mysql.js
@@ -0,0 +1,8 @@
+module.exports = mysql_config = {
+    host: <db_host_ip>,
+    user: <db_user>,
+    password: <db_password>,
+    database: live,
+    multipleStatements: true,
+    connectionLimit : 10
+}
