96d7726fabd94c280269412081c0d7d7e331b330
Adam <a.zartin@mail.utoronto.ca>
2019-07-22 15:55:49 -0400

    startup scripts

--- installMySQL.sh
+++ installMySQL.sh
@@ -8,5 +8,7 @@ echo "\n \n Setting Up Live Schema"
 #Create the schema meant for live application
 mysql < ./databaseScripts/setupSchema.sql
 echo "\n \n Securing Accounts"
+#Startup script 
+mysql < ./databaseScripts/start_script.sql
 #Create password for root and user app
 sh ./databaseScripts/secureSQL.sh
