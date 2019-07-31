40401ef2ab9a757cf925d9c014e8fb5e2c802b45
root@138.197.151.255 <root@138.197.151.255>
2019-07-31 15:09:08 +0000

    dashboard now copies folder to /var/www/

--- dashboardBuild.sh
+++
@@ -1,9 +0,0 @@
-dashboardFolder=$1
-
-echo "\n Building Produciton Build of $dashboardFolder \n"
-
-cd $dashboardFolder
-npm install
-sleep 5
-npm run build
-
---
+++ setupFrontendApp.sh
@@ -0,0 +1,24 @@
+dashboardFolder=$1
+ipAddress=`wget http://ipecho.net/plain -O - -q ; echo`
+
+echo "\n Building Produciton Build of $dashboardFolder \n"
+
+cd $dashboardFolder
+echo "" > "build.txt"
+sudo npm install
+sudo npm run build >> "build.txt"
+cd ..
+
+#Copy folder
+sudo cp -r "./$dashboardFolder/build" "/var/www/"
+
+#Restart NGINX
+sudo service nginx restart
+sudo systemctl restart nginx
+sudo systemctl reload nginx
+
+#Provide link address
+LINK="$ipAddress"
+
+echo "\n \n \n"
+echo "Website up at: \n$LINK\n"
