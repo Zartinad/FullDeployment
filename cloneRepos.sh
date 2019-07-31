f579108c0500c577d9e020b0eb8f7650bfa49aae
Adam <a.zartin@mail.utoronto.ca>
2019-07-31 10:44:42 -0400

    repos are modular

--- cloneRepos.sh
+++ cloneRepos.sh
@@ -1,3 +1,7 @@
-git clone --branch feature-multiple-currency https://github.com/MarkdaleMGMT/transaction-methods.git
-git clone --branch develop https://github.com/MarkdaleMGMT/frontend-server.git
-git clone --branch develop https://github.com/MarkdaleMGMT/frontend-app.git
+backend=$1
+frontendserver=$2
+frontendapp=$3
+
+git clone --branch "$backend" https://github.com/MarkdaleMGMT/transaction-methods.git
+git clone --branch "$frontendserver" https://github.com/MarkdaleMGMT/frontend-server.git
+git clone --branch "$frontendapp" https://github.com/MarkdaleMGMT/frontend-app.git
