8601a773796b33062783f7a00a45ee7a60bcf48c
root@165.227.35.11 <root@165.227.35.11>
2019-08-08 02:05:37 +0000

    finalTouches

--- setupFrontendServer.sh
+++ setupFrontendServer.sh
@@ -4,14 +4,14 @@ frontend=$1
 #ip_address=`wget http://ipecho.net/plain -O - -q ; echo`
 #echo $ip_address
 
-touch ./$1/config/component/mysql.js
-node inputFrontend.js ./$1/config/component/mysql
+touch ./$1/config/components/mysql.js
+node inputFrontend.js ./$1/config/components/mysql
 
 echo "" > pm2Delete.txt
 pm2 delete "frontend_server"
 
 cd $1
-sudo npm install
+sudo npm install --verbose
 pm2 start -f frontend_server.js
 cd ..
 
